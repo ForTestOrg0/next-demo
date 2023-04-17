bfde9435d4889ff3bfdd004b5a6483c572bd8748
WoeOm <1015996366@qq.com>
2023-04-17 09:29:11 +0800

    feat: outline button

--- src/ui/Button/Button.tsx
+++ src/ui/Button/Button.tsx
@@ -3,14 +3,20 @@
 import React, { HTMLAttributes } from 'react';
 import clsx from 'clsx';
 
-const Flex: React.FC<HTMLAttributes<HTMLButtonElement>> = ({ children, className, ...props }) => (
+interface ButtonProps {
+  outline?: boolean;
+}
+
+const Button: React.FC<ButtonProps & HTMLAttributes<HTMLButtonElement>> = ({ children, className, outline, ...props }) => (
   <button
     type="button"
-    className={clsx('rounded-md bg-white px-2.5 py-1.5 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50', className)}
+    className={clsx('rounded px-8 py-1.5 text-sm border border-sub-network',
+      { 'text-sub-network bg-sub-white': outline },
+      { 'text-sub-white bg-sub-network': !outline }, className)}
     {...props}
   >
     {children}
   </button>
 );
 
-export default Flex;
+export default Button;
