c6b420a8d3997640f06c290e17ac3e9cf6498a23
WoeOm <1015996366@qq.com>
2023-04-18 11:02:24 +0800

    style: ui

--- src/ui/Box/Boundary.tsx
+++ src/ui/Box/Boundary.tsx
@@ -2,7 +2,7 @@ import React, { HTMLAttributes } from 'react';
 import clsx from 'clsx';
 
 const Boundary: React.FC<HTMLAttributes<HTMLDivElement>> = ({ children, className, ...props }) => (
-  <div className={clsx('rounded bg-sub-white border border-sub-b4 shadow-module p-2', className)} {...props}>
+  <div className={clsx('rounded bg-sub-white border border-sub-b4 shadow-module py-2 px-5', className)} {...props}>
     {children}
   </div>
 );
