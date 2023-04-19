4e4e242c058e34f78e96af3a7f45db7432b5c933
Carl <hongtao19950703@163.com>
2023-04-19 14:56:54 +0800

    feat: add eina font (#1)

--- src/pages/layout.tsx
+++ src/pages/layout.tsx
@@ -1,14 +1,28 @@
 import Footer from '@/components/Footer';
 import Header from '@/components/Header';
 import Head from 'next/head'
+import localFont from 'next/font/local'
 
+const eina = localFont({
+  src: [
+    {
+      path: '../fonts/Eina01.otf',
+      weight: '400',
+    },
+    {
+      path: '../fonts/Eina01-SemiBold.otf',
+      weight: '600',
+    },
+  ],
+  variable: '--font-eina'
+})
 export default function RootLayout({
   children,
 }: {
   children: React.ReactNode
 }) {
   return (
-    <>
+    <div className={`${eina.variable} font-sans`}>
       <Head>
         <title>Subscan | Aggregate Substrate ecological network high-precision Web3 explorer</title>
         <meta name="description" data-vmid="description" content="Subscan | Substrate ecological explorer: Search, analyze, and visualize blockchain that occurs on Substrate based network (Polkadot, Kusama, Moonbeam, Acala, Darwinia), such as extrinsics, transfers, accounts, tokens, prices, staking, PLO and other activities." />
@@ -16,6 +30,6 @@ export default function RootLayout({
       <Header />
       {children}
       <Footer reactVersion="v18" nextVersion='v13' />
-    </>
+    </div>
   )
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -9,6 +9,9 @@ module.exports = {
   ],
   theme: {
     extend: {
+      fontFamily: {
+        sans: ['var(--font-eina)']
+      },
       colors: {
         sub: {
           black: '#302B3C',
