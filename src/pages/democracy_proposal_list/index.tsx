73f9cd96da0770c0bf2d6124298fa6cd6d5ba0a7
WoeOm <1015996366@qq.com>
2023-04-20 13:41:57 +0800

    pagination (#2)
    * feat: optimize Pagination
    
    * feat: add pagination for democracy

--- src/pages/democracy_proposal_list/index.tsx
+++ src/pages/democracy_proposal_list/index.tsx
@@ -1,4 +1,4 @@
-import { Boundary, PageContent, Container, Text, TabsServer, Table, Th, Td, Tr, Link, LinkRouter, Button, Pagination } from '@/ui';
+import { Boundary, PageContent, Container, Text, Table, Th, Td, Tr, LinkRouter, Pagination, Flex } from '@/ui';
 import { GetServerSideProps, InferGetServerSidePropsType } from 'next';
 import { getDemocracyProposals, GetDemocracyProposalsDataProps } from '@/utils/api';
 import { PAGE_ROW } from '@/config/constants';
@@ -13,12 +13,18 @@ const statusMap = {
   historical: 'historical',
 }
 
-export const getServerSideProps: GetServerSideProps<{ data: GetDemocracyProposalsDataProps, type: keyof typeof orderFieldMap }> = async (context) => {
+export const getServerSideProps: GetServerSideProps<{
+  data: GetDemocracyProposalsDataProps,
+  type: keyof typeof orderFieldMap,
+  page: number,
+}> = async (context) => {
   const tab = (context.query.status || '')?.toString() as (keyof typeof orderFieldMap);
+  const page = parseInt(context.query.page as string) || 1;
+
   const type = typeof orderFieldMap[tab] === 'undefined' ? 'waiting' : tab;
-  const orderField = orderFieldMap[type] || orderFieldMap.waiting;
-  const status = statusMap[type] || statusMap.waiting;
-  const data = await getDemocracyProposals(context.req.headers.host || '', { "row": PAGE_ROW, "page": 0, "order_field": orderField, "status": status });
+  const orderField = typeof orderFieldMap[type] === 'undefined' ? orderFieldMap.waiting : orderFieldMap[type];
+  const status = typeof statusMap[type] === 'undefined' ? statusMap.waiting : statusMap[type];
+  const data = await getDemocracyProposals(context.req.headers.host || '', { "row": PAGE_ROW, "page": page - 1, "order_field": orderField, "status": status });
 
   if (!data || data.code !== 0) {
     return {
@@ -30,11 +36,12 @@ export const getServerSideProps: GetServerSideProps<{ data: GetDemocracyProposal
     props: {
       data: data.data,
       type,
+      page: page,
     },
   }
 }
 
-export default function Layout({ data, type }: InferGetServerSidePropsType<typeof getServerSideProps>) {
+export default function Layout({ data, type, page }: InferGetServerSidePropsType<typeof getServerSideProps>) {
   return (
     <PageContent>
       <Container className='flex-1'>
@@ -68,7 +75,9 @@ export default function Layout({ data, type }: InferGetServerSidePropsType<typeo
             </tbody>
           </Table>
         </Boundary>
-        <Pagination total={data.count} pageSize={PAGE_ROW} current={1}/>
+        <Flex className='mt-5 flex-row-reverse'>
+          <Pagination total={data.count} pageSize={PAGE_ROW} current={page} urlRender={(_page) => `/democracy_proposal_list?status=${type}&page=${_page}`} />
+        </Flex>
       </Container>
     </PageContent >
   );
--- src/ui/Pagination/Pagination.tsx
+++ src/ui/Pagination/Pagination.tsx
@@ -7,14 +7,25 @@ interface Props extends BareProps {
   current: number;
   total: number;
   pageSize: number;
-  urlRender?: (page: number) => string;
+  urlRender: (page: number) => string;
 }
 
-const normalCls = 'relative inline-flex items-center px-4 py-2 text-sm font-semibold text-gray-900 ring-1 ring-inset ring-gray-300 hover:bg-gray-50 focus:z-20 focus:outline-offset-0';
-const currentCls = 'relative z-10 inline-flex items-center bg-indigo-600 px-4 py-2 text-sm font-semibold text-white focus:z-20 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600';
+const normalCls = 'relative inline-flex items-center px-4 py-2 rounded text-sm text-sub-b2 ring-1 ring-inset ring-gray-300 hover:bg-gray-50 focus:z-20 focus:outline-offset-0';
+const currentCls = 'relative z-10 inline-flex items-center px-4 py-2 rounded ring-1 ring-inset ring-sub-network text-sm text-sub-network focus:z-20 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600';
+const disableCls = 'relative inline-flex items-center px-4 py-2 rounded text-sm text-gray-700 ring-1 ring-inset ring-gray-300 focus:outline-offset-0';
+const boxCls = 'isolate inline-flex -space-x-px rounded-md shadow-sm space-x-1';
 
-const PreviousBtn = () => {
-  return (<LinkRouter href="#" className="relative inline-flex items-center rounded-l-md px-2 py-2 text-gray-400 ring-1 ring-inset ring-gray-300 hover:bg-gray-50 focus:z-20 focus:outline-offset-0">
+const PreviousBtn = ({ current, minPageNumber, urlRender }: { current: number, minPageNumber: number, urlRender: (page: number) => string; }) => {
+  if (current <= minPageNumber) {
+    return (<span className="relative inline-flex items-center rounded px-2 py-2 text-gray-400 ring-1 ring-inset ring-gray-300">
+      <span className="sr-only">Previous</span>
+      <svg className="h-5 w-5" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
+        <path fillRule="evenodd" d="M12.79 5.23a.75.75 0 01-.02 1.06L8.832 10l3.938 3.71a.75.75 0 11-1.04 1.08l-4.5-4.25a.75.75 0 010-1.08l4.5-4.25a.75.75 0 011.06.02z" clipRule="evenodd" />
+      </svg>
+    </span>)
+  }
+
+  return (<LinkRouter href={urlRender(current - 1)} className="relative inline-flex items-center rounded px-2 py-2 text-sub-network ring-1 ring-inset ring-gray-300 hover:bg-gray-50 focus:z-20 focus:outline-offset-0">
     <span className="sr-only">Previous</span>
     <svg className="h-5 w-5" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
       <path fillRule="evenodd" d="M12.79 5.23a.75.75 0 01-.02 1.06L8.832 10l3.938 3.71a.75.75 0 11-1.04 1.08l-4.5-4.25a.75.75 0 010-1.08l4.5-4.25a.75.75 0 011.06.02z" clipRule="evenodd" />
@@ -22,8 +33,17 @@ const PreviousBtn = () => {
   </LinkRouter>)
 }
 
-const NextBtn = () => {
-  return (<LinkRouter href="#" className="relative inline-flex items-center rounded-r-md px-2 py-2 text-gray-400 ring-1 ring-inset ring-gray-300 hover:bg-gray-50 focus:z-20 focus:outline-offset-0">
+const NextBtn = ({ current, maxPageNumber, urlRender }: { current: number, maxPageNumber: number, urlRender: (page: number) => string; }) => {
+  if (current >= maxPageNumber) {
+    return (<span className="relative inline-flex items-center rounded px-2 py-2 text-gray-400 ring-1 ring-inset ring-gray-300">
+      <span className="sr-only">Next</span>
+      <svg className="h-5 w-5" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
+        <path fillRule="evenodd" d="M7.21 14.77a.75.75 0 01.02-1.06L11.168 10 7.23 6.29a.75.75 0 111.04-1.08l4.5 4.25a.75.75 0 010 1.08l-4.5 4.25a.75.75 0 01-1.06-.02z" clipRule="evenodd" />
+      </svg>
+    </span>)
+  }
+
+  return (<LinkRouter href={urlRender(current + 1)} className="relative inline-flex items-center rounded px-2 py-2 text-sub-network ring-1 ring-inset ring-gray-300 hover:bg-gray-50 focus:z-20 focus:outline-offset-0">
     <span className="sr-only">Next</span>
     <svg className="h-5 w-5" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
       <path fillRule="evenodd" d="M7.21 14.77a.75.75 0 01.02-1.06L11.168 10 7.23 6.29a.75.75 0 111.04-1.08l4.5 4.25a.75.75 0 010 1.08l-4.5 4.25a.75.75 0 01-1.06-.02z" clipRule="evenodd" />
@@ -36,27 +56,55 @@ const NumberBtn = ({ isCurrent, pageNumber, urlRender }: { isCurrent: boolean, p
 }
 
 const Pagination: React.FC<Props> = ({ children, className, current, total, pageSize, urlRender = (page) => '' }) => {
-  console.log(111, total, pageSize)
   const totalPage = Math.ceil(total / pageSize);
   if (totalPage <= 6) {
-    return <nav className={clsx('isolate inline-flex -space-x-px rounded-md shadow-sm', className)}>
-      <PreviousBtn />
+    return <nav className={clsx(boxCls, className)}>
+      <PreviousBtn current={current} minPageNumber={1} urlRender={urlRender} />
       {Array.from(Array(totalPage).keys()).map((item, index) => {
         const pageNumber = index + 1;
         return (<NumberBtn key={`Pagination-${pageNumber}`} isCurrent={current === pageNumber} pageNumber={pageNumber} urlRender={urlRender} />)
       })}
-      <NextBtn />
+      <NextBtn current={current} maxPageNumber={totalPage} urlRender={urlRender} />
     </nav>
   }
 
-  return <nav className={clsx('isolate inline-flex -space-x-px rounded-md shadow-sm', className)}>
-    <PreviousBtn />
-    {Array(totalPage).map((item, index) => {
-      const pageNumber = index + 1;
-      return (<LinkRouter key={`Pagination-${pageNumber}`} href={urlRender(pageNumber)}>{pageNumber}</LinkRouter>)
-    })}
-    <NextBtn />
-  </nav>
+  if (current < 4) {
+    return (<nav className={clsx(boxCls, className)}>
+      <PreviousBtn current={current} minPageNumber={1} urlRender={urlRender} />
+      <NumberBtn isCurrent={current === 1} pageNumber={1} urlRender={urlRender} />
+      <NumberBtn isCurrent={current === 2} pageNumber={2} urlRender={urlRender} />
+      <NumberBtn isCurrent={current === 3} pageNumber={3} urlRender={urlRender} />
+      <NumberBtn isCurrent={current === 4} pageNumber={4} urlRender={urlRender} />
+      <span className={disableCls} >...</span>
+      <NumberBtn isCurrent={current === totalPage} pageNumber={totalPage} urlRender={urlRender} />
+      <NextBtn current={current} maxPageNumber={totalPage} urlRender={urlRender} />
+    </nav>)
+  }
+
+  if (current >= 4 && current < totalPage - 2) {
+    return (<nav className={clsx(boxCls, className)}>
+      <PreviousBtn current={current} minPageNumber={1} urlRender={urlRender} />
+      <NumberBtn isCurrent={current === 1} pageNumber={1} urlRender={urlRender} />
+      <span className={disableCls} >...</span>
+      <NumberBtn isCurrent={false} pageNumber={current - 1} urlRender={urlRender} />
+      <NumberBtn isCurrent={true} pageNumber={current} urlRender={urlRender} />
+      <NumberBtn isCurrent={false} pageNumber={current + 1} urlRender={urlRender} />
+      <span className={disableCls} >...</span>
+      <NumberBtn isCurrent={false} pageNumber={totalPage} urlRender={urlRender} />
+      <NextBtn current={current} maxPageNumber={totalPage} urlRender={urlRender} />
+    </nav>)
+  }
+
+  return (<nav className={clsx(boxCls, className)}>
+    <PreviousBtn current={current} minPageNumber={1} urlRender={urlRender} />
+    <NumberBtn isCurrent={current === 1} pageNumber={1} urlRender={urlRender} />
+    <span className={disableCls} >...</span>
+    <NumberBtn isCurrent={totalPage - 3 === current} pageNumber={totalPage - 3} urlRender={urlRender} />
+    <NumberBtn isCurrent={totalPage - 2 === current} pageNumber={totalPage - 2} urlRender={urlRender} />
+    <NumberBtn isCurrent={totalPage - 1 === current} pageNumber={totalPage - 1} urlRender={urlRender} />
+    <NumberBtn isCurrent={totalPage === current} pageNumber={totalPage} urlRender={urlRender} />
+    <NextBtn current={current} maxPageNumber={totalPage} urlRender={urlRender} />
+  </nav>)
 };
 
 export default Pagination;
