032561e155519737494069e20764fb861c5a2d47
WoeOm <1015996366@qq.com>
2023-04-20 17:09:15 +0800

    feat: add storybook (#4)

---
+++ src/ui/Pagination/Pagination.stories.ts
@@ -0,0 +1,22 @@
+import type { Meta, StoryObj } from "@storybook/react";
+
+import { Pagination } from "./";
+
+const meta: Meta<typeof Pagination> = {
+  title: "UI/Pagination",
+  component: Pagination,
+  tags: ["autodocs"],
+  argTypes: {},
+};
+
+export default meta;
+type Story = StoryObj<typeof Pagination>;
+
+export const Primary: Story = {
+  args: {
+    current: 20,
+    total: 400,
+    pageSize: 10,
+    urlRender: (page) => `/test?page=${page}`,
+  },
+};
