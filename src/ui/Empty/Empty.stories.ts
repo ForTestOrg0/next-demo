d0faee481828930a589e41d3b1533e16915bebe4
Jay <lygm1062@gmail.com>
2023-05-04 15:54:08 +0800

    feat: empty component (#27)

---
+++ src/ui/Empty/Empty.stories.ts
@@ -0,0 +1,21 @@
+import type { Meta, StoryObj } from "@storybook/react";
+
+import { Empty } from ".";
+
+// More on how to set up stories at: https://storybook.js.org/docs/react/writing-stories/introduction
+const meta: Meta<typeof Empty> = {
+  title: "UI/Empty",
+  component: Empty,
+  tags: ["autodocs"],
+  argTypes: {},
+};
+
+export default meta;
+type Story = StoryObj<typeof Empty>;
+
+// More on writing stories with args: https://storybook.js.org/docs/react/writing-stories/args
+export const Primary: Story = {
+  args: {
+    description: "Oops, no data",
+  },
+};
---
+++ src/ui/Empty/Empty.tsx
@@ -0,0 +1,19 @@
+import { FC, ReactNode } from "react";
+import Image from "next/image";
+import noData from "./assets/no-data.png";
+
+export interface EmptyProps {
+  description?: ReactNode;
+  className?: string;
+}
+
+const Empty: FC<EmptyProps> = ({ description, className }) => {
+  return (
+    <div className={`flex flex-col items-center gap-4 ${className}`}>
+      <Image alt="no data" src={noData} />
+      {description}
+    </div>
+  );
+};
+
+export default Empty;
