548bb026b6fcb03463a1e215f3d87c90a98ee4bc
Jay <lygm1062@gmail.com>
2023-05-04 17:16:41 +0800

    feat: storage utils (#29)

---
+++ src/utils/storage/common.ts
@@ -0,0 +1,46 @@
+export const setStore = (key: string, data: string | boolean | number | null | undefined | unknown[] | Object) => {
+  if (data === null || data === undefined) {
+    localStorage.removeItem(key);
+  } else {
+    let value: string = "";
+
+    switch (typeof data) {
+      case "boolean":
+      case "number":
+        value = `${data}`;
+        break;
+      case "string":
+        value = data;
+        break;
+      case "object":
+        value = JSON.stringify(data);
+      default:
+        throw new Error('type does not support');
+    }
+
+    localStorage.setItem(key, value);
+  }
+};
+
+export const getStore = <T = boolean | number | string | Object | null>(key: string): T => {
+  const value = localStorage.getItem(key);
+  let result: unknown = null;
+
+  if (value === "false") {
+    result = false;
+  } else if (value === "true") {
+    result = true;
+  } else if (value) {
+    if (/^[0-9]+$/.test(value)) {
+      result = Number(value);
+    } else {
+      try {
+        result = JSON.parse(value);
+      } catch {
+        result = value;
+      }
+    }
+  }
+
+  return result as T;
+};
---
+++ src/utils/storage/theme.ts
@@ -0,0 +1,9 @@
+import { setStore, getStore } from "./common";
+
+const KEY = "theme";
+
+export const setTheme = (theme?: "light" | "dark" | null) => {
+  setStore(KEY, theme);
+};
+
+export const getTheme = () => getStore<"light" | "dark" | null>(KEY);
