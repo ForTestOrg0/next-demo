c56111148391d711757dd634ebc7dd8ec29177c1
carlhong <acarlhong@gmail.com>
2023-05-15 09:56:14 +0800

    feat: add ga4

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,4 +1,6 @@
 import type { ReactElement, ReactNode } from 'react'
+import { useEffect } from 'react';
+import { useRouter } from 'next/router';
 import type { GetServerSideProps, NextPage } from 'next'
 import type { AppProps } from 'next/app'
 import localFont from 'next/font/local'
@@ -7,6 +9,14 @@ import '@/styles/globals.css';
 import '@/ui/Tabs/styles.scss';
 import { GetTokenUniqueIdProps, getTokenUniqueId } from '@/utils/api'
 import BigNumber from 'bignumber.js'
+import ReactGA from "react-ga4";
+
+// add global ga4
+// https://www.npmjs.com/package/react-ga4
+const isProd = process.env.NODE_ENV === "production";
+if (isProd) {
+  ReactGA.initialize('G-RLSZTY8RF0');
+}
 
 // This config is required for number formatting
 // https://mikemcl.github.io/bignumber.js/#toS
@@ -46,6 +56,20 @@ export const getInitialProps: GetServerSideProps<{
 }
 
 export default function MyApp({ Component, pageProps }: AppPropsWithLayout) {
+  const router = useRouter();
+  useEffect(() => {
+    const handleRouteChange = (url: string, options: { shallow: boolean; }) => {
+      if (isProd) {
+        ReactGA.send({ hitType: "pageview", page: url });
+      } else {
+        console.log(`App is changing to ${url} ${options.shallow ? "with" : "without"} shallow routing`);
+      }
+    }
+    router.events.on('routeChangeComplete', handleRouteChange);
+    return () => {
+      router.events.off('routeChangeComplete', handleRouteChange);
+    }
+  }, [router.events]);
   // Use the layout defined at the page level, if available
   const getLayout = Component.getLayout ?? ((page) => page)
 
